9bbbe5430eec8073c98d457e367f3af1a026480d
lyreal666 <2713151713@qq.com>
2018-07-10 08:15:47 +0800

    index complete

---
+++ company/subjectExam/components/textIcon/textIcon.js
@@ -0,0 +1,12 @@
+Component({
+    properties: {
+        imgSrc: {
+            type: String,
+            value: '/common/imgs/quesMark.jpg'
+        },
+        iconText: {
+            type: String,
+            value: '文字图标默认文字'
+        }
+    }
+})
---
+++ company/subjectExam/pages/index/index.js
@@ -0,0 +1,9 @@
+Page({
+  data: {
+      orderIcons: [
+          {imgSrc: '/common/imgs/chapterPractice.png', iconText: '章节练习'},
+          {imgSrc: '/common/imgs/wrongQues.png', iconText: '错题汇总'},
+          {imgSrc: '/common/imgs/randPractice.png', iconText: '随机练习'},
+      ]
+  }
+})
